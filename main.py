2ef4f8261adf8cbd9443f40f4e427ae7ff6825bc
Luke Ottey <lukeottey@Lukes-MacBook-Pro.local>
2022-04-04 08:41:59 -0400

    update

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 import json
 import argparse
 import bibtexparser
+from bidict import bidict
 from functools import partial
 from ordered_set import OrderedSet
 from collections import namedtuple
 
+from abbrev import ABBREV_MAP
 
 
 _TEX_IN = """\\documentclass[acmlarge]{acmart}
@@ -26,13 +28,14 @@ Luke Ottey
 \\newpage"""
 
 
+
 class TexRelatedFileNotFound(Exception):
     ...
 
 
 class CitedItem(namedtuple("CitedItem", "id title location")):
     def to_latex(self):
-        return f"\\item {self.title} " + "\\cite{" + self.id + "}"
+        return f"\\item {self.title} " + "\\cite{" + self.id + "} "
 
     def __repr__(self):
         return f"{self.title} [{self.id}]"
@@ -40,8 +43,8 @@ class CitedItem(namedtuple("CitedItem", "id title location")):
 def parse_args():
     def path_fixer(p, suffix, must_exist):
         if not p.startswith("tex_stuff/"):
-            assert p.count("/") == 0, p
             p = f"tex_stuff/{p}"
+        assert p.count("/") == 1, p
         
         if not p.endswith(suffix):
             p = f"{p}{suffix}"
@@ -51,24 +54,28 @@ def parse_args():
         return p
  
     p = argparse.ArgumentParser()
-    p.add_argument("-b", "--bibfiles", type=partial(path_fixer, suffix=".bib", must_exist=True), nargs="+", default=["bibliography"])
+    p.add_argument("-b", "--bibfile", type=partial(path_fixer, suffix=".bib", must_exist=True), default="bibliography")
     p.add_argument("-f", "--fout", type=partial(path_fixer, suffix=".tex", must_exist=False), default="core")
     p.add_argument("-t", "--toc", type=partial(path_fixer, suffix=".json", must_exist=True), default="toc")
+    p.add_argument("-k", "--key", type=str, default="none")
     return p.parse_args()
 
 
 class IterBibTex:
-    def __init__(self, bibpaths):
-        self.bibpaths = tuple(bibpaths)
+    def __init__(self, bibpath, allowed_keys):
+        self.bibpath = bibpath
+        self.allowed_keys = allowed_keys
 
     def __iter__(self):
-        for p in self.bibpaths:
-            with open(p) as bibfile:
-                bibs = bibtexparser.load(bibfile)
-            for bt in bibs.entries:
-                loc = tuple(bt.get("keywords", "no-category").split(","))
-                yield CitedItem(
-                    id=bt["ID"], location=loc, title=bt["title"])        
+        with open(self.bibpath) as bibfile:
+            bibs = bibtexparser.load(bibfile)
+        for bt in bibs.entries:
+            locs = list(map(lambda loc: "::".join([ABBREV_MAP.get(_k, _k) for _k in loc.split("::")]), bt.get("keywords", "no-category").split(","))) 
+            for loc in locs:
+                assert any(key.startswith(loc) for key in self.allowed_keys), loc
+            yield CitedItem(
+                id=bt["ID"], location=tuple(locs), title=bt["title"]) 
+
 
 class CitationsMap:
     def __init__(self, allowed_keys):
@@ -77,24 +84,32 @@ class CitationsMap:
             self.__d[k] = OrderedSet()
 
     def _check_key(self, k):
-        if k not in self.__d:
+        k = "::".join([ABBREV_MAP.get(_k, _k) for _k in k.split("::")])  
+        if k in self.__d:
+            return k
+        if not any(key.startswith(k) for key in list(self.__d)):
             raise KeyError(f"input key '{k}' not one of:\n[{', '.join(list(self.__d))}]")
+        return k
+    
 
     def __add(self, __k, cited_item: CitedItem):
-        self._check_key(__k)
+        __k = self._check_key(__k)
         self.__d[__k].add(cited_item)
 
     def add(self, cited_item: CitedItem):
         for loc in cited_item.location:
             self.__add(loc, cited_item)
 
-    def get(self, __k):
-        self._check_key(__k)
-        return tuple(self.__d[__k])
-
-    def nested_sections(self):
-        pm = dict()
-        return pm
+    def get(self, __k, mlvl=False, skip_check=False):
+        if not skip_check:
+            __k = self._check_key(__k)
+        if not mlvl:
+            return tuple(self.__d[__k])
+        v = OrderedSet()
+        for ___k in filter(lambda x: x.startswith(__k), self.toc()):
+            for item in self.__d[___k]:
+                v.add(item)
+        return tuple(v)
 
     def toc(self):
         return tuple(self.__d)
@@ -124,6 +139,9 @@ class CitationsMap:
     def __repr__(self):
         return repr(self.__d)
 
+def filter_wonocat(citations):
+    return tuple(filter(lambda item: item.location != ("no-category",), citations))
+
 def fill_tex_body(cite_map):
 
     mlvl_keys = dict()
@@ -139,21 +157,33 @@ def fill_tex_body(cite_map):
     for key in filter(lambda k: k.count("::") == 0, iter(cite_map)):
         mlvl_keys[key] = make_sections(key)
     tex_body = []
-
+    used_keys = set()
+    section_per_depth = ("section", "subsection", "subsubsection", "paragraph", "subparagraph")
     def write_tex(d, depth=0):
         for k, v in d.items():
+            if k in used_keys:
+                continue
             tab = "\t" * k.count('::')
             if depth == 0:
-                tex_body.append(tab + "\\addtocontents{toc}{\\setcounter{tocdepth}{3}}")
-            tex_body.append(f"{tab}\\{k.count('::') * 'sub'}section" + "{" + k.split("::")[-1] + "}")
-            tex_body.append(f"{tab}\\begin" + "{" + "enumerate" + "}")
+                tex_body.append(tab + "\\addtocontents{toc}{\\setcounter{tocdepth}{6}}")
+            header = f"{tab}\\{section_per_depth[depth]}" + "{" + k.split("::")[-1] + "}"
+            tex_body.append(header)
+            cited_items = tuple(cite_map.get(k, mlvl=False, skip_check=True))
+            
             if isinstance(v, dict):
                 write_tex(v, depth + 1)
             else:
                 assert v is None, v
-            for cited_item in cite_map.get(k):
-                tex_body.append(tab + "\t" + cited_item.to_latex())
+            begin_enum = f"{tab}\\begin" + "{" + "enumerate" + "}"
+            tex_body.append(begin_enum)
+            for cited_item in cited_items:
+                tex_body.append(tab + "\t" + cited_item.to_latex() + "\n")
+            if tex_body[-1] == begin_enum and tex_body[-2] == header:
+                tex_body.append(tab + "\t" + "\\item")
+            
             tex_body.append(f"{tab}\\end" + "{" + "enumerate" + "}")
+
+            used_keys.add(k)
     write_tex(mlvl_keys)
     return list(tex_body)
 
@@ -169,16 +199,23 @@ def main():
             raise TexRelatedFileNotFound(path)
 
     cite_map = CitationsMap.from_json(args.toc)
-    bibtex_iter = IterBibTex(args.bibfiles)
+    bibtex_iter = IterBibTex(args.bibfile, tuple(cite_map.toc()))
     for citation in iter(bibtex_iter):
         cite_map.add(citation)
-    tex_code = _TEX_IN.split("\n") + fill_tex_body(cite_map) + \
-        ["\\newpage", "\\bibliographystyle{ACM-Reference-Format}"] + \
-            ["\\bibliography{" + bibfile[bibfile.index("/") + 1: bibfile.index(".")] + "}" for bibfile in args.bibfiles] + \
-                ["\\end{document}"]
-    with open(args.fout, "w") as fout:
-        fout.write("\n".join(tex_code))
-    print(f"\nWritten to LaTeX file {args.fout}\n")
+    
+    if args.key != "none":
+        print(f"------ {args.key.upper()} -------\n")
+        for item in filter_wonocat(cite_map.get(cite_map._check_key(args.key), mlvl=True)):
+            print([" | ".join(item.location)])
+            print(item.to_latex(), "\n")
+    else:
+        tex_code = _TEX_IN.split("\n") + fill_tex_body(cite_map) + \
+            ["\\newpage", "\\bibliographystyle{ACM-Reference-Format}"] + \
+                ["\\bibliography{" + args.bibfile[args.bibfile.index("/") + 1: args.bibfile.index(".")] + "}", "\\end{document}"]
+
+        with open(args.fout, "w") as fout:
+            fout.write("\n".join(tex_code))
+        print(f"\nWritten to LaTeX file {args.fout}\n")
 
 
 
